779947fb6884a1563e85d2e6c8e48affa992be41
Jed Presley <c_brickman@hotmail.com>
2023-07-22 14:57:21 -0500

    Steps 1 and 2.1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// TODO: add code here
+
+
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+        });
+    });
+});
