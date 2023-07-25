ffc7c3cae8a827af1437ca8c6bda6e9879c5361f
Jed Presley <c_brickman@hotmail.com>
2023-07-24 19:19:39 -0500

    Bonus 1

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
             const container = document.getElementById("container");
+            json = json.sort(function(b, a) {
+                return a.hoursInSpace - b.hoursInSpace });
             for (let i=0; i<json.length; i++) {
             container.innerHTML += `
                 
@@ -18,8 +20,10 @@ window.addEventListener("load", function() {
                         </div>
                         <img class="avatar" img src=${json[i].picture}>
                     </div>
+                    
             `};
-                        
+            
+        
         });
     });
             
